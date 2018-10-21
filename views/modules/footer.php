9ba96914b61a0a9bb56f577372e929d0f31ab558
Fernando-Devtrix <luis.fernando.mtz.glez0970@gmail.com>
2018-10-20 22:15:08 -0500

    Adding system modules

---
+++ views/modules/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2018</strong>
+	Todos los derechos reservados
+</footer>
---
+++ views/modules/header.php
@@ -0,0 +1,70 @@
+<header class="main-header">
+	
+<!--=====================================
+=            LOGO			            =
+======================================-->
+	<a href="" class="logo">
+		
+		<!-- mini logo -->
+		<span class="logo-mini">
+			
+			<img src="views/img/template/icono-blanco.png" class="img-responsive" style="padding: 10px">
+
+		</span>
+
+		<!-- normal logo -->
+
+		<span class="logo-lg">
+			
+			<img src="views/img/template/logo-blanco-lineal.png" class="img-responsive" style="padding: 10px 0px">
+
+		</span>
+
+	</a>
+
+	<!--=====================================
+	=            LOGO			            =
+	======================================-->
+	<nav class="navbar navbar-static-top" role="navigation">
+
+		<!-- Nav Button -->
+		
+		<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+
+			<span class="sr-only">Toggle navigation</span>
+
+		</a>
+
+		<!-- Perfil de usuario -->
+
+		<div class="navbar-custom-menu">
+			
+			<ul class="nav navbar-nav">
+				<li class="dropdown user user-menu">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<img src="views/img/users/default/anonymous.png" alt="anonymous img" class="user-image">
+
+						<span class="hidden-xs">Admin User</span>
+
+					</a>
+			
+					<!-- Dropdown-toggle -->
+
+					<ul class="dropdown-menu">
+						
+						<li class="user-body">
+							<div class="pull-right">
+								<a href="#" class="btn btn-default btn-flat">Exit</a>
+							</div>
+						</li>
+
+					</ul>
+
+				</li>
+			</ul>
+
+		</div>
+
+	</nav>
+
+</header>
